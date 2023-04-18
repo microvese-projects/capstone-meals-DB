ff9e0d821a7f9712d64f53a96c75717c52d4c6ac
Benson Njuguna <njugunab655@gmail.com>
2023-04-18 19:29:50 +0300

    Add form and comments section

--- src/modules/displayMealData.js
+++ src/modules/displayMealData.js
@@ -21,6 +21,21 @@ const displayMealData = ({
             ${instructions.map((instruction) => `<p>${instruction}</p>`)}
           </div>
         </div>
+        <div>
+          <h3>Comments (<span id="comments-count">2</span>)</h3>
+          <ul id="previous-comments">
+            <li>01/02/2023 Ben: I love the meal!</li>
+            <li>01/02/2023 Rushema: I love the meal!</li>
+          </ul>
+        </div>
+        <form>
+          <h3 id="form-heading">Add a comment</h3>
+          <label for="name">Your name</label>
+          <input type="text" id="name" minlength="3" placeholder="Your name" required> 
+          <label for="insights">Your Insights</label>
+          <textarea id="insights" minlength="3" placeholder="Your insights" minlength="3" required></textarea>
+          <button type="Submit">Comment</button>
+        </form>
   `;
   popup.innerHTML = data;
 };
